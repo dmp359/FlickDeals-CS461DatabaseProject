51d07712af6a822297529635300d5e0ab43ed25e
dmp359 <dmp359@drexel.edu>
2018-05-09 09:38:06 -0400

    Our diagram requires accounts and customers to be in one table. Also fixed type errors

--- schema.sql
+++ schema.sql
@@ -1,36 +1,20 @@
-drop table Customers;
-drop table Accounts;
+drop table Users;
 
-create table Customers (
-	customerId integer primary key,
+create table Users (
+  customerId integer primary key,
 	age integer,
 	firstName varchar(128),
 	lastName varchar(128),
 	gender varchar(1),
-	accountId integer unique not null,
-	foreign key accountId references Accounts(accountId)
-);
-
-create table Accounts (
-	accountId integer primary key,
-	phoneNum integer not null,
+	accountId integer not null unique, 
+	phoneNum varchar(10) not null,
 	email varchar(128) not null,
-	password varchar(128) not null,
-	customerId integer unique not null,
-	foreign key customerId references Customers(customerId) on delete cascade
+	password varchar(128) not null
 );
 
-insert into Customers values (6184, 18, 'John', 'Finkle', 'M', 613595567);
-insert into Customers values (1743, 25, 'Phil', 'Wrinkle', 'M', 3155444588);
-insert into Customers values (9148, 38, 'Sarah', 'Lint', 'F', 21158711);
-insert into Customers values (3215, 27, 'Rob', 'Zambonie', 'M', 38275017);
-insert into Customers values (5262, 50, 'Jill', 'Heart', 'F', 87483962);
-insert into Customers values (8532, 21, 'Will', 'Dwightson', 'M', 89739173);
-
-
-insert into Accounts values (613595567, 1895321832, 'john.finkle@gmail.com', 'password123', 6184);
-insert into Accounts values (3155444588, 5935290053, 'philextreme@comcast.net', '1F93jhfcS', 1743);
-insert into Accounts values (21158711, 8875314431, 'sarahsmiles@hotmail.com', 'cat43run92', 9148);
-insert into Accounts values (38275017, 2672539982, 'RobbieBobbie234@hotmail.com', 'ihaveadog', 3215);
-insert into Accounts values (87483962, 2155555555, 'iheartjill43@gmail.com', 'jillywillynilly', 5262);
-insert into Accounts values (89739173, 88725808524, 'williamnot43@gmail.com', '2Fj2i3lolp', 8532);
+insert into Users values (6135, 18, 'John', 'Finkle', 'M', 6184, '1873498920', 'john.finkle@gmail.com', 'password123');
+insert into Users values (3155, 25, 'Phil', 'Wrinkle', 'M', 1743, '5935290053', 'philextreme@comcast.net', '1F93jhfcS');
+insert into Users values (2115, 38, 'Sarah', 'Lint', 'F', 9148, '5259482958', 'sarahsmiles@hotmail.com', 'cat43run92');
+insert into Users values (3827, 27, 'Rob', 'Zambonie', 'M', 3215, '2672539982', 'RobbieBobbie234@hotmail.com', 'ihaveadog');
+insert into Users values (8748, 50, 'Jill', 'Heart', 'F', 5262, '2153878738', 'iheartjill43@gmail.com', 'jillywillynilly');
+insert into Users values (8973, 21, 'Will', 'Dwightson', 'M', 8532, '1234567895', 'williamnot43@gmail.com', '2Fj2i3lolp');
