ca4e91c25192ba2c875936da85f5afc66ed0238f
Dan Perlman <dmp359@drexel.edu>
2018-05-12 00:05:32 -0400

    More comments/cleanup

--- src/UI.py
+++ src/UI.py
@@ -5,16 +5,28 @@ from IPython.display import display
 from Users import User
 from Registrar import Registrar
 
-
 #Define Widgets.
+
+# Search bar text
 search_bar = widgets.Text(
     placeholder='Search',
     description='Find Deal:',
     disabled=False,display='flex'
 )
 
-search_button = widgets.Button(description="Search",display='flex'
+# Search bar button
+search_button = widgets.Button(description="Search")
+
+# Welcome banner and picture
+welcome_banner = widgets.HTML(
+    value='''
+        <link rel="stylesheet" href="styles.css">
+        Hi and welcome to <b>Flick Deals</b>
+        <img src="http://images.clipartpanda.com/money-clipart-money-pics-free.png" height=50 width = 50>
+        <h1> Check out today's hottest deals! </h1>
+    ''',
 )
+
 
 # This list will be created programatically eventually
 # Filled in with title/description and clickable image from db
@@ -26,17 +38,7 @@ deal_HTML_items = [
     '<div class="grid-item">Deal description 5</div>',
     '<div class="grid-item">Deal description 6</div>',
 ]
-
-welcome_banner = widgets.HTML(
-    value='''
-        <link rel="stylesheet" href="styles.css">
-        Hi and welcome to <b>Flick Deals</b>
-        <img src="http://images.clipartpanda.com/money-clipart-money-pics-free.png" height=50 width = 50>
-        <h1> Check out today's hottest deals! </h1>
-    ''',
-)
-
-gridItems = ''.join(deal_HTML_items)
+gridItems = ''.join(deal_HTML_items) # Convet deal_HTML_items to a string
 deal_grid = widgets.HTML(
     value='''
         <div class="grid-container">
@@ -44,18 +46,17 @@ deal_grid = widgets.HTML(
         </div>
     '''.format(gridItems=gridItems)
 )
+
 # Search bar and button horizontally next to each other
 search_component = widgets.HBox([search_bar, search_button])
 
 # Vertically arrange welcome banner, search component, and deal grid to create welcome page
 welcome_page = widgets.VBox([welcome_banner, search_component, deal_grid])
 
-
-
 # Tab component
 pages = widgets.Tab()
 
- # Set what widget is shown on each page
+# Set what widget is shown on each page. To start, only show welcome page.
 pages.children = [welcome_page]
 
 # Set name of tabs
@@ -81,6 +82,7 @@ def run_deal_search_query(sender):
         '''.format(response=response),
     )]
     
+    # Switch to search result page
     pages.selected_index = 1
 
 # Query handlers
