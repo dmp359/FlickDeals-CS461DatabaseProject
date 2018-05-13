db9e44f60e8bc28f3c734fe5b252af562d56e75c
Dan Perlman <dmp359@drexel.edu>
2018-05-12 20:55:40 -0400

    moved schema file

--- src/schema.sql
+++ src/schema.sql
@@ -12,7 +12,7 @@ create table Users (
 	password varchar(128) not null
 );
 
-insert into Users values (6135, 18, 'JOOHNN', 'Finkle', 'M', 6184, '1873498920', 'john.finkle@gmail.com', 'password123');
+insert into Users values (6135, 18, 'John', 'Finkle', 'M', 6184, '1873498920', 'john.finkle@gmail.com', 'password123');
 insert into Users values (3155, 25, 'Phil', 'Wrinkle', 'M', 1743, '5935290053', 'philextreme@comcast.net', '1F93jhfcS');
 insert into Users values (2115, 38, 'Sarah', 'Lint', 'F', 9148, '5259482958', 'sarahsmiles@hotmail.com', 'cat43run92');
 insert into Users values (3827, 27, 'Rob', 'Zambonie', 'M', 3215, '2672539982', 'RobbieBobbie234@hotmail.com', 'ihaveadog');
